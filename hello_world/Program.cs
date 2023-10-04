7fbd97d2d149d38d7bbe6b8344d3901deefeb631
MrMaximusis <gggamermaximusis@gmail.com>
2023-10-04 21:25:39 +0300

    Программа модифицированна в вывод имени

--- hello_world/Program.cs
+++ hello_world/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("А вот и я!");
+
+Console.WriteLine("Hello, World from MrMaximusis");
